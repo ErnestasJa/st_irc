4d4fc5ef48b79d2f85151ea1ff229aa7a4651788
serengeor <serengeor@gmail.com>
2014-04-15 02:30:48 +0300

    !kps = global kappa counter, user lists Signed-off-by: serengeor <serengeor@gmail.com>

--- pyirc_connection.py
+++ pyirc_connection.py
@@ -7,13 +7,21 @@ class PyIRC:
 
     def __init__(self):
         self.connected = False
+        self.dc_callback = None
+
+    def is_connected(self):
+        return self.connected
     
     def connect(self, TCP_ADDR, TCP_PORT):
         self.connection_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.connection_socket.connect((socket.gethostbyname(TCP_ADDR), TCP_PORT))
         self.connected = True
+
+        try:
+            self.connection_socket.connect((socket.gethostbyname(TCP_ADDR), TCP_PORT))
+        except Exception as e:
+            print("Something's wrong with %s. Exception type is %s" % (address, e))
+            self.connected = False
         
-    
     def send_msg(self, msg):
         if self.connected == False:
             raise Exception("Tried to send message while not being connected to any sever.")
@@ -21,11 +29,18 @@ class PyIRC:
         self.connection_socket.send(bytes(msg+"\n", 'UTF-8'))
         
     def parse_messages(self, my_callback):
-        BUFFER_SIZE = 1024
+        BUFFER_SIZE = 4096
         readbuffer=""
         
         while 1:
-            readbuffer = readbuffer + self.connection_socket.recv(BUFFER_SIZE).decode("utf-8")
+            try:
+                cur_data = self.connection_socket.recv(BUFFER_SIZE)
+            except Exception as e:
+                print(str(e))
+                self.connected = False
+                break;
+            
+            readbuffer = readbuffer + cur_data.decode("utf-8")
             temp = str.split(readbuffer, "\n")
             readbuffer = temp.pop( )
 
@@ -36,7 +51,7 @@ class PyIRC:
                 if(tokens[0]=="PING"):
                     self.send_msg("PONG " + str(tokens[1]) + "\r\n")
                 else:
-                    GLib.idle_add(my_callback,line)
+                    GLib.idle_add(my_callback,line, priority = GLib.PRIORITY_HIGH )
             
 
     def disconnect(self):
--- pyirc_gui.py
+++ pyirc_gui.py
@@ -2,9 +2,7 @@
 
 import re
 import pickle
-from gi.repository import Gtk
-from gi.repository import Gdk
-from gi.repository import GLib
+from gi.repository import Gtk, Gdk, GLib, Pango
 from pyirc_connection import PyIRC
 
 try:
@@ -17,6 +15,35 @@ import threading
 
 GLib.threads_init()
 
+class Channel:
+    def __init__(self, name):
+        self.users = Gtk.ListStore(str)
+        self.chat_log = Gtk.TextBuffer()
+        self.bold_tag = self.chat_log.create_tag("b", weight=Pango.Weight.BOLD)
+        self.name = name
+
+    def echo(self, message, *tags):
+        self.chat_log.insert_with_tags(self.chat_log.get_end_iter(),message+"\n",*tags)
+
+    def echo_no_newline(self, message, *tags):
+        self.chat_log.insert_with_tags(self.chat_log.get_end_iter(),message,*tags)
+
+    def add_user(self, user):
+        self.users.append(user)
+
+    def get_bold_tag(self):
+        return self.bold_tag
+
+    def remove_user(self, user):
+        for row in self.users:
+            if row[0] == user[0]:
+                self.users.remove(row.iter)
+                break
+
+    def clear_users(self):
+        self.users.clear()
+    
+
 class Task:
     def __init__(self, my_task, my_cb):
        self.function = my_task
@@ -24,7 +51,6 @@ class Task:
        self.thread = None
 
     def _start(self):
-        
         self.function(self.my_cb)
 
     def start(self):
@@ -40,6 +66,7 @@ class Task:
 class PyIRC_Application:
 
     def __init__(self):
+        self.kps = KappaStats(self)
 
         self.builder = Gtk.Builder()
         self.builder.add_from_file("pyirc_interface.glade")
@@ -64,6 +91,7 @@ class PyIRC_Application:
         self.cj_join_btn        = self.builder.get_object("cj_join_btn")
         self.cj_cancel_btn      = self.builder.get_object("cj_cancel_btn")
         self.send_btn           = self.builder.get_object("send_btn")
+        self.user_list          = self.builder.get_object("user_list")
 
         #signals
         self.window.connect("delete-event", Gtk.main_quit)
@@ -79,8 +107,8 @@ class PyIRC_Application:
         self.cj_cancel_btn.connect("clicked", self.on_cj_cancel_btn_clicked)
         self.entry.connect("key-press-event", self.on_entry_key_pressed_enter)
 
-        #Notebook stuff
-        self.textbuffers = {}
+        #Channels
+        self.channels = {}
 
         #create irc connection object
         self.irc = PyIRC()
@@ -89,6 +117,8 @@ class PyIRC_Application:
         # show main window
         self.load_saved_con_info()
         self.window.show_all()
+        self.parse_cmd_re = re.compile("^(:(?P<prefix>\S+) )?(?P<command>\S+)( (?!:)(?P<params>.+?))?( :(?P<trail>.+))?$")
+        self.parse_user_list_cmd_re = re.compile("^(?P<name>\S+) = (?P<channel>\S+)$")
 
     def on_delete_event(self, widget, event, data =None):
         if self.irc.connected:
@@ -98,44 +128,83 @@ class PyIRC_Application:
         Gtk.main_quit()
 
     def on_msg(self, msg):
-        prog = re.compile("^(:(?P<prefix>\S+) )?(?P<command>\S+)( (?!:)(?P<params>.+?))?( :(?P<trail>.+))?$")
-        groups = prog.match(msg)
-
+        groups = self.parse_cmd_re.match(msg)
+        #print(msg)
+        
         output = ""
 
-        print(msg)
+        if (groups.group('command') is not None) and (groups.group('params') is not None):
+            if groups.group('command') == "PRIVMSG":
+                nick = groups.group('prefix').partition('!')[0]
+                channel = groups.group('params')
+                
+                if(self.channels_notebook.get_current_page()!=-1):
+                    page = self.channels_notebook.get_nth_page(self.channels_notebook.get_current_page())
+                    active_channel = self.channels_notebook.get_tab_label_text(page).strip()
+                    if channel == active_channel:
+                        self.textview.scroll_to_iter(self.textview.get_buffer().get_end_iter(),0.0,False,0,0)
+                        
+                if channel in self.channels:
+                    ch = self.channels[channel]
+                    ch.echo_no_newline(nick,ch.get_bold_tag())
+                    ch.echo(": " + groups.group('trail'))
+                    self.kps.on_message(nick,channel,groups.group('trail'))
 
-        if (groups.group('command') is not None) and (groups.group('params') is not None) and (groups.group('command') == "PRIVMSG"):
-            nick = groups.group('prefix').partition('!')[0]
-            output = nick + ": " + groups.group('trail')
-            channel = groups.group('params')
-            
-            if(self.channels_notebook.get_current_page()!=-1):
-                page = self.channels_notebook.get_nth_page(self.channels_notebook.get_current_page())
-                active_channel = self.channels_notebook.get_tab_label_text(page).strip()
-                if channel == active_channel:
-                    self.textview.scroll_to_iter(self.textview.get_buffer().get_end_iter(),0.0,False,0,0)
-                    
-            self.echo_msg(channel,output)
-            
+            elif groups.group('command') == "353":
+                match = self.parse_user_list_cmd_re.match(groups.group('params'))
+                ch_name = match.group('channel')
+                ch = self.channels[ch_name]
+
+                users = groups.group('trail').split()
+                
+                for usr in users:
+                    ch.add_user([usr])
+
+            elif groups.group('command') == "JOIN":
+                nick = groups.group('prefix').partition('!')[0]
+                ch_name = groups.group('params').strip()
+                ch = self.channels[ch_name]
+                
+                ch.add_user([nick])
+
+            elif groups.group('command') == "PART":
+                nick = groups.group('prefix').partition('!')[0]
+                ch_name = groups.group('params').strip()
+                ch = self.channels[ch_name]
+                
+                ch.remove_user([nick])
+
+        return False;
+                          
 
     def on_switch_channel(self, widget, page, page_number, data = None):
         name = self.channels_notebook.get_tab_label_text(page).strip()
-        if name in self.textbuffers:
-            self.textview.set_buffer(self.textbuffers[name])
+        if name in self.channels:
+            self.textview.set_buffer(self.channels[name].chat_log)
+            self.user_list.set_model(self.channels[name].users)
         
 
     def on_connect_btn_clicked(self, widget):
+        if self.irc.is_connected():
+            parent = None
+            md = Gtk.MessageDialog(parent, 0, Gtk.MessageType.WARNING,Gtk.ButtonsType.CLOSE,
+                "Can't connect to more than one server.\nPlease first disconnect.")
+            md.run()
+            md.destroy()
+            return
+            
         self.connection_dialog.show_all();
 
     def on_join_channel_btn_clicked(self, widget):
-        if self.irc.connected:
+        if self.irc.is_connected():
             self.join_channel_dialog.show_all();
-
-    def echo_msg(self, channel, msg):
-        if channel in self.textbuffers:
-            buf = self.textbuffers[channel]
-            buf.insert(buf.get_end_iter(),msg+"\n")
+        else:
+            parent = None
+            md = Gtk.MessageDialog(parent, 0, Gtk.MessageType.WARNING,Gtk.ButtonsType.CLOSE,
+                "Can't join a channel while not connected to server.\nPlease connect to a server first.")
+            md.run()
+            md.destroy()
+            return
 
     def evaluate_entry_box(self):
         if self.irc.connected:
@@ -144,14 +213,15 @@ class PyIRC_Application:
             page_num = self.channels_notebook.get_current_page()
 
             if page_num == -1 :
-                print("Not in channel.")
                 return
 
             page = self.channels_notebook.get_nth_page(page_num)
             channel = self.channels_notebook.get_tab_label_text(page).strip()
 
+            if channel in self.channels:
+                self.channels[channel].echo("> "+self.nick+": "+text)
+
             self.irc.send_msg("PRIVMSG " + channel + " :" + text)
-            self.echo_msg(channel,"> "+self.nick+": "+text)
             self.entry.set_text("")
 
     def on_send_btn_clicked(self, widget):
@@ -163,6 +233,10 @@ class PyIRC_Application:
 
     def on_disconnect_btn_clicked(self, widget):
         if self.irc.connected:
+            self.channels = {}
+            while self.channels_notebook.get_current_page() != -1:
+                self.channels_notebook.remove_page(self.channels_notebook.get_current_page())
+            self.channels_notebook.show_all()
             self.irc.disconnect()
     
     def on_cd_save_btn_clicked(self, widget):
@@ -206,13 +280,30 @@ class PyIRC_Application:
     def on_cj_join_btn_clicked(self, widget):
         channel_name = self.builder.get_object("cj_channel_name_entry").get_text().strip()
         self.irc.send_msg("JOIN " + channel_name)
-        self.textbuffers[channel_name]=Gtk.TextBuffer()
+        self.channels[channel_name]=Channel(channel_name)
         self.join_channel_dialog.hide();
         self.channels_notebook.insert_page(Gtk.Frame(),Gtk.Label(channel_name),0)
         self.channels_notebook.show_all()
 
     def on_cj_cancel_btn_clicked(self, widget):
         self.join_channel_dialog.hide();
+
+
+class KappaStats:
+    def __init__(self, irc_app):
+        self.kappa_count = 0
+        self.irc_app = irc_app
+        self.kappa_re = re.compile("(?=((^|\s)Kappa(\s|$)))")
+        
+    def on_message(self, user, channel, msg):
+        if msg.strip() == "!kappacount":
+            ch = self.irc_app.channels[channel]
+            ch.echo("<<<<<<<<<<<<<<<<<<<<<<<<<  Counting Kappas  >>>>>>>>>>>>>>>>>>>")
+            self.irc_app.irc.send_msg("PRIVMSG " + channel + " :Current kappa count: " + str(self.kappa_count))
+        
+        
+        self.kappa_count += len(self.kappa_re.findall(msg))
+    
 
 app = PyIRC_Application()
 Gtk.main()
